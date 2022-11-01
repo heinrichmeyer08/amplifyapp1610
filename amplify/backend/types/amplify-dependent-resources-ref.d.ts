80206c44f91ea35bfc9c733236a0d0dc8769f93d
heinrichmeyer08 <heinrichmeyer08@gmail.com>
2022-11-01 13:07:30 +0100

    added auth

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "amplifyapp16102576f668": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
